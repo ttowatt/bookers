a6ace9e6f9f4f5e79ee57ccab15f890a1da128a3
towa6868 <tanakatowa852@gmail.com>
2025-08-13 04:44:16 +0000

    [In progress]1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to bookers_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookers/books' => 'books#new'
+  post 'books' => 'books#create'
+  get 'bookers/books' => 'book#index'
+  get 'bookers/books/boo' => 'books#show' #booは例
+  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'bookers' => 'homes#top'
 end
