af0aa6633dc6a1828f4a6a89edfbc93e4151e2ba
towa6868 <tanakatowa852@gmail.com>
2025-08-13 15:00:18 +0000

    [In progress]5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
       render 'new'
@@ -21,18 +22,21 @@ end
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @books = Book.find(params[:id])
+    @book = Book.new
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated"
     redirect_to book_path(book.id)  
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed"
     redirect_to '/books'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :books
   get 'bookers/books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'bookers' => 'homes#top'
+  root 'homes#top'
 end
