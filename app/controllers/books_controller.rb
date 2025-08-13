d13772f5940527baa8bec33f7d7fa1f80b18f6cb
towa6868 <tanakatowa852@gmail.com>
2025-08-13 13:53:44 +0000

    [In progress]4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,15 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   #def new @book = Book.new end
   
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
     else
       render 'new'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  get 'bookers/books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'bookers' => 'homes#top'
 end
